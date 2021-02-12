b905f2c22c908cb87a1a0297e1d28b49dca0ced4
beka <b.zhanaikhanov@mycar.kz>
2021-02-12 11:58:02 +0600

    elementui import optimized

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
-import ElementUI from 'element-ui';
+import {Select, Dialog, Checkbox, Button, Slider, Option} from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 
-Vue.use(ElementUI);
+Vue.use(Select);
+Vue.use(Dialog);
+Vue.use(Checkbox);
+Vue.use(Button);
+Vue.use(Slider);
+Vue.use(Option);
 Vue.config.productionTip = false
 
 new Vue({
